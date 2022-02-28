aba2e389f0b8809e77c0afd04631c374423b0eb5
rjleonam <sousa.manoel@icloud.com>
2022-02-27 20:30:58 -0400

    Variaveis e metodos static

---
+++ src/com/loiane/cursojava/aula34/TestaCalculadora.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula34;
+
+public class TestaCalculadora {
+	public static void main(String[] args) {
+		/*
+		 * MinhaCalculadora calc = new MinhaCalculadora(); calc.soma(1, 2);
+		 */
+		
+		System.out.println(MinhaCalculadora.soma(1, 3));
+	}
+}
